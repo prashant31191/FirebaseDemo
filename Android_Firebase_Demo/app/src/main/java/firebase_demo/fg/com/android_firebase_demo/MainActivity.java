2b3401dc1bf72ce50fc62eb8932b67629642d4d3
Dhaval Mehta <dhaval.mehta@eliteinfoworld.com>
2017-04-24 18:03:26 +0530

    First commit

--- Android_Firebase_Demo/app/src/main/java/firebase_demo/fg/com/android_firebase_demo/MainActivity.java
+++ Android_Firebase_Demo/app/src/main/java/firebase_demo/fg/com/android_firebase_demo/MainActivity.java
@@ -1,7 +1,10 @@
 package firebase_demo.fg.com.android_firebase_demo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -10,10 +13,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import firebase_demo.fg.com.android_firebase_demo.storage.StorageMainActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
     private DataListAdapter listAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +31,17 @@ public class MainActivity extends AppCompatActivity {
 
         // Init user list
         ListView list = (ListView) this.findViewById(R.id.dataList);
+        Button btnStorgae = (Button) this.findViewById(R.id.btnStorgae);
+
         this.listAdapter = new DataListAdapter(this, R.layout.list_view_cell);
         list.setAdapter(listAdapter);
 
+        btnStorgae.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, StorageMainActivity.class));
+            }
+        });
 
         // Add listener
         mDatabase.addChildEventListener(new FirebaseEventListener(this.listAdapter));
